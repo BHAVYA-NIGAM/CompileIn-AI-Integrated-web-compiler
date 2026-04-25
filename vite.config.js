13d9caa81b2594108f5389fa03f6f4ab26542aa4
Your Name <you@example.com>
2026-04-25 12:24:54 +0530

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    // tailwindcss(),
-  ],
-  base: "/compilein/",
+  plugins: [react()],
+  base: "/",   // ✅ FIXED
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
